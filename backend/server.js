3ddd69ca0201f7f8f66e78703ef267d021e9290e
paul <waycaosi1994@gmail.com>
2021-03-07 15:39:58 +0700

    Create Server BackEnd

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import moongoose from 'mongoose';
+import cors from 'cors';
+
+const app = express();
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  'mongodb+srv://waycaosi:GIAbao1998@booksstore.dnjc2.mongodb.net/BooksStore?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+moongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
+
+moongoose.set('useFindAndModify', false);
